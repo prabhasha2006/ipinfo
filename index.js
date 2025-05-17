492e3e3d24684a146f3c56a3d994bf896e1abf9c
Evelocore <kumuthuprabhasha@gmail.com>
2025-05-17 23:55:23 +0530

    dotenv

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const geoip = require('geoip-lite')
 const requestIp = require('request-ip')
 const cors = require('cors')
 const morgan = require('morgan')
+require('dotenv').config()
 
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 // Middleware
 app.use(cors())
